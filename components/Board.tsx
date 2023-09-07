6fa762926f5003f0192ace4d29d449e87d0a611c
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-07 20:49:57 +0100

    Create base layer for Drag and Drop components in Board

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function Board() {
   return (
-    <div>Board</div>
+    <DragDropContext>
+        {/* You have to give the droppable an ID and then a direction */}
+        <Droppable droppableId='board' direction='horizontal' type='column'>
+            {/* Access child of this component to  render column */}
+            {(provided) => (
+                <div>
+                    {/* Rendering all columns */}
+
+                </div>
+            )}
+
+        </Droppable>
+
+    </DragDropContext>
   )
 }
 

2ea15326cd5f1367dea1dcdb966de991449e36bf
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-08 20:52:43 +0100

    UI Improvement to Card Columns

--- components/Column.tsx
+++ components/Column.tsx
@@ -10,12 +10,12 @@ interface Props {
 
 //use this to convert column text(id) to better form
 const idToColumnText: {
-    [key in TypedColumn]: string;
-  } = {
-    todo: "To Do",
-    inprogress: "In Progress",
-    done: "Done",
-  };
+  [key in TypedColumn]: string;
+} = {
+  todo: "To Do",
+  inprogress: "In Progress",
+  done: "Done",
+};
 function Column({ id, todos, index }: Props) {
   return (
     <Draggable draggableId={id} index={index}>
@@ -32,9 +32,14 @@ function Column({ id, todos, index }: Props) {
               <div
                 {...provided.droppableProps}
                 ref={provided.innerRef}
-                className={`pb-2 p-2 rounded-2xl shadow-sm ${snapshot.isDraggingOver ? 'bg-green-700': 'bg-white/50'}`}
+                className={`pb-2 p-2 rounded-2xl shadow-sm ${
+                  snapshot.isDraggingOver ? "bg-green-700" : "bg-white/50"
+                }`}
               >
-                <h2>{idToColumnText[id]}{" "}</h2>
+                <h2 className="flex justify-between font-bold text-xl p-2">
+                  {idToColumnText[id]} {/*  */}
+                  <span className="text-gray-500 bg-grey-200 rounded-full px-2 py-1 text-sm font-normal"></span>
+                </h2>
               </div>
             )}
           </Droppable>

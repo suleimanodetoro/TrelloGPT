35f82502a3cc0c6dbd4ace292d96b1cf39fd2342
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-09 00:42:21 +0100

    Add ToDo Card Items without functionality

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,24 +1,47 @@
-'use client'
+"use client";
 
-import React from 'react'
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import React from "react";
 import { useEffect, useState } from "react";
 import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
 } from "react-beautiful-dnd";
 type Props = {
-    todo: Todo;
-    index: number;
-    id: TypedColumn;
-    innerRef: (element: HTMLElement | null) => void;
-    draggableProps: DraggableProvidedDraggableProps;
-    dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
-  };
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+};
 
-function TodoCard({index, id, todo,}: Props) {
+function TodoCard({
+  index,
+  id,
+  todo,
+  innerRef,
+  draggableProps,
+  dragHandleProps,
+}: Props) {
   return (
-    <div>TodoCard</div>
-  )
+    <div
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+    >
+        <div className="flex items-center justify-between p-5">
+            {/* Task title */}
+            <p>{todo.title}</p>
+            {/* delete task button */}
+            <button className="text-red-500 hover:text-red-800">
+                {/* "X" ICON */}
+                <XCircleIcon className="ml-5 h-8 w-8"/>
+            </button>
+        </div>
+    </div>
+  );
 }
 
-export default TodoCard
+export default TodoCard;
